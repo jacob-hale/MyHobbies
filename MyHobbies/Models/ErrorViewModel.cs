bf3ba06947b57d340b64db5152088fbbc78399d4
Jacob Hale <Jchale23@byui.edu>
2026-02-04 13:00:11 -0700

    Add project files.

---
+++ MyHobbies/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyHobbies.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
