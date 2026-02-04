db8b94c061dca68d25b8d76267a2f044cfe8d070
Jacob Hale <Jchale23@byui.edu>
2026-02-04 14:41:43 -0700

    make the rate calculator work

--- MyHobbies/wwwroot/js/site.js
+++ MyHobbies/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function calculateCost() {
+    const hours = parseFloat($('#hours').val()) || 0;
+
+    const total = hours * 15;
+
+    if (hours > 0) {
+        $('#totalCost').val(`$${total.toFixed(2)}`);
+    }
+    else {
+        alert("Please enter a valid number of hours.")
+    }
+}
