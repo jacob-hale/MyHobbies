6674631bc42855a1f09a470261a28c9a367dd19d
Jacob Hale <Jchale23@byui.edu>
2026-02-04 15:04:47 -0700

    beans

--- MyHobbies/wwwroot/js/site.js
+++ MyHobbies/wwwroot/js/site.js
@@ -2,6 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+// Calculate cost based on hours input (jQuery)
 function calculateCost() {
     const hours = parseFloat($('#hours').val()) || 0;
 
